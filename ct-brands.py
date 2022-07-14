0f234378b18a364bdb32a977cc55a1dae1c8bbfe
tuck1s <steve.tuck@sparkpost.com>
2022-07-14 12:13:34 +0100

    Working website-based lookup

--- ct-brands.py
+++ ct-brands.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-import os, sys, csv, argparse, time, inspect
+import os, sys, csv, argparse, time
 from competitivetracker import CompetitiveTracker
 from competitivetracker.exceptions import CompetitiveTrackerAPIException
-from competitivetracker.base import Resource
+from urllib.parse import urlparse
+from typing import NamedTuple
 
 def eprint(*args, **kwargs):
     """
@@ -11,7 +12,8 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def rate_limiting_in(self, err):
+def rate_limiting_in(err):
+    retry_wait_secs = 2 # Seconds to back off when rate-limiting seen
     if err.status == 503 or err.status == 429:
         msg = err.errors[0]
         if 'Account Over Rate Limit' in msg:
@@ -20,84 +22,44 @@ def rate_limiting_in(self, err):
             return False
         if 'Account Over Queries Per Second Limit' in msg:
             eprint('.. pausing for per-second query rate-limiting ..')
-            time.sleep(self.retry_wait_secs)
+            time.sleep(retry_wait_secs)
             return True
     # Any other
     return False
 
 
-# Child class, with (some of) the methods of the parent class, but automatically handling rate-limiting retries
-class RetryingCompetitiveTracker():
-    def __init__(self, *args, **kwargs):
-
-        self.call_log = {}
-        self.retry_wait_secs = 2 # Seconds to back off when rate-limiting seen
-        self.log_call(inspect.currentframe().f_code.co_name)
-        # Create an upstream service to call
-        self.up = CompetitiveTracker(api_key=key)
-        self.core = self.Core(self.up.core) # Expose the inner classes as per https://www.geeksforgeeks.org/inner-class-in-python/
-        self.intelligence = self.Intelligence(self.up.intelligence)
-        self.domain_info = self.Domain_info(self.up.domain_info)
-
-    # Log the number of calls made to each function
-    def log_call(self, name):
-        if name in self.call_log:
-            self.call_log[name] += 1
+def org_domain(url):
+    """
+    Allow the http:// or https:// to be present or absent
+    """
+    parts = urlparse(url)
+    if parts.scheme and parts.netloc:
+        domain = parts.netloc
+    else:
+        # this is not strictly a "correct" URL as it's missing the scheme. Try adding it.
+        parts = urlparse('http://' + url)
+        if parts.scheme and parts.netloc:
+            domain = parts.netloc
         else:
-            self.call_log[name] = 1
+            return None
 
-    def call_stats(self):
-        return self.call_log
+    # Strip back a URL with www. prefix to just the organizational domain, for better matching
+    return domain.removeprefix('www.')
 
-    class Core:
-        def __init__(self, up):
-            self.up = up
-            self.discover = self.Discover(self.up.discover)
-            self.companies = self.Companies(self.up.companies)
 
-        class Discover:
-            def __init__(self, up):
-                self.up = up
-
-            def search_companies(self, **kwargs):
-                # self.log_call(inspect.currentframe().f_code.co_name)
-                return self.up.search_companies(**kwargs)
-
-        class Companies:
-            def __init__(self, up):
-                self.up = up
-
-            def get_all_company_brands(self, *args, **kwargs):
-                # self.log_call(inspect.currentframe().f_code.co_name)
-                return self.up.get_all_company_brands(*args, **kwargs)
-
-    class Intelligence:
-        def __init__(self, up):
-            self.up = up
-            self.brand = self.Brand(self.up.brand)
-
-        class Brand:
-            def __init__(self, up):
-                self.up = up
-
-            def Get_top_domains(self, *args, **kwargs):
-                # self.log_call(inspect.currentframe().f_code.co_name)
-                return self.up.get_top_domains(*args, **kwargs)
-
-    class Domain_info:
-        def __init__(self, up):
-            self.up = up
-
-        def Get_brand_volume_and_esps(self, *args, **kwargs):
-            # self.log_call(inspect.currentframe().f_code.co_name)
-            return self.up.get_brand_volume_and_esps(*args, **kwargs)
+def is_url_like(url):
+    """
+    Recognize URLs if they start with http:// https://, or start with wwww.
+    """
+    return urlparse(url).netloc != '' or url.startswith('www.')
 
 
 # -----------------------------------------------------------------------------------------
 
+'''
 def get_company_info(ct, co):
     """
-    Get information for a company.
+    Get information for a company, from a human-readable name
     """
     while True:
         try:
@@ -113,21 +75,30 @@ def get_company_info(ct, co):
                 eprint(err)
                 return None
 
-    top_company = company_results.get('companies')
-    if len(top_company) < 1:
+    companies = company_results.get('companies')
+    if len(companies) < 1:
         return None
 
     # Choose the first entry as the top company match
-    top_company_id = top_company[0].get('id')
-    top_company_name = top_company[0].get('name')
+    top_company_id = companies[0].get('id')
+    top_company_name = companies[0].get('name')
     if not top_company_id:
         return None
 
+    x = make_company_results('name match', top_company_name, top_company_id)
+    return x
+'''
+
+
+def make_company_results(ct, website, company_name, company_id):
+    """
+    Provide company results in a simplified dict format
+    """
     while True:
         try:
             global api_cb
             api_cb += 1
-            brand_results = ct.core.companies.get_all_company_brands(ct, companyId=top_company_id)
+            brand_results = ct.core.companies.get_all_company_brands(companyId=company_id)
             break
         except CompetitiveTrackerAPIException as err:
             if rate_limiting_in(err):
@@ -161,20 +132,8 @@ def get_company_info(ct, co):
         if domains:
             domain_name_vol = { d['name']: d['projectedVolume'] for d in domains }
             domain_name_list = domain_name_vol.keys()
-            # Get all ESPs for the list of sending domains
-            query_period = 90
-            while True:
-                try:
-                    global api_bv
-                    api_bv += 1
-                    volume_avg_and_esps = ct.domain_info.get_brand_volume_and_esps(domains=domain_name_list, timePeriod=query_period)
-                    break
-                except CompetitiveTrackerAPIException as err:
-                    if rate_limiting_in(err):
-                        continue
-                    else:
-                        eprint(err)
-                        return None
+
+            volume_avg_and_esps = get_vol_avg_and_esps(ct, domain_name_list)
 
             for d, proj_vol in domain_name_vol.items():
                 i = volume_avg_and_esps[d] # results are indexed by name
@@ -182,7 +141,8 @@ def get_company_info(ct, co):
                     esplist = [ n['name'] for n in j['esps'] ]
                     espString = ','.join(esplist)
                     result.append( {
-                        'company': top_company_name,
+                        'org_domain': website,
+                        'company': company_name,
                         'brand': brand_name,
                         'domain': d,
                         'volume': proj_vol,
@@ -190,7 +150,8 @@ def get_company_info(ct, co):
         else:
             # This is a company/brand with no sending domains. Return blank results
             result.append( {
-                'company': top_company_name,
+                'org_domain': website,
+                'company': company_name,
                 'brand': brand_name,
                 'domain': None,
                 'volume': None,
@@ -199,11 +160,63 @@ def get_company_info(ct, co):
     return result
 
 
+def get_vol_avg_and_esps(ct, domain_name_list):
+    """
+    Get all ESPs for the list of sending domains. The volume is averaged for the time period, over all the domains, so it's
+    not as granular as other endpoints.
+    """
+    while True:
+        try:
+            global api_bv
+            api_bv += 1
+            # Get data for X months back to present day
+            volume_avg_and_esps = ct.domain_info.get_brand_volume_and_esps(domains=domain_name_list, timePeriod=3, precision='months')
+            break
+        except CompetitiveTrackerAPIException as err:
+            if rate_limiting_in(err):
+                continue
+            else:
+                eprint(err)
+                return None
+    return volume_avg_and_esps
+
+
+def get_domain_info(ct, company_site):
+    """
+    Get information for an organizational domain.
+    """
+    while True:
+        try:
+            company_results = ct.core.graph.get_company_from_domain(domainName=org_domain(company_site))
+            break
+        except CompetitiveTrackerAPIException as err:
+            if rate_limiting_in(err):
+                continue
+            else:
+                eprint(err)
+                return None
+
+    company_name = company_results['name']
+    company_id = company_results['id']
+
+
+    # TODO: could walk Child Companies and/or Parent Companies too - skip for now
+    # company_results.get('childCompanies')
+    # company_results.get('parentCompany')
+
+    x = make_company_results(ct, company_site, company_name, company_id)
+    return x
+
+
+# -----------------------------------------------------------------------------------------
+# Main code
+# -----------------------------------------------------------------------------------------
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Simple command-line tool to fetch company brand, domain, volume and ESPs information from Competitive Tracker')
 
-    parser.add_argument('files', metavar='file', type=argparse.FileType('r'), default=[sys.stdin], nargs="*", help='files containing a list of companies to process. If omitted, reads from stdin.')
+    parser.add_argument('files', metavar='file', type=argparse.FileType('r'), default=[sys.stdin], nargs="*", help='files containing a list of company names or website(s) to process. If omitted, reads from stdin.')
     parser.add_argument('-o', '--outfile', metavar='outfile.csv', type=argparse.FileType('w'), default=sys.stdout, help='output filename (CSV format), must be writable. If omitted, prints to stdout.')
     args = parser.parse_args()
 
@@ -211,7 +224,7 @@ if __name__ == "__main__":
     if key == None:
         print('Please define CT_API_KEY env variable before running.')
         exit(1)
-    ct = RetryingCompetitiveTracker(key)
+    ct = CompetitiveTracker(key)
 
     # TEMP: instrument the calls
     api_sc = 0
@@ -226,8 +239,15 @@ if __name__ == "__main__":
         inh = csv.reader(infile)
         done_header = False
         for line in inh:
-            for company in line:
-                result = get_company_info(ct, company)
+            for company_site in line:
+                # map URL-like names into organizational domains
+                if is_url_like(company_site):
+                    result = get_domain_info(ct, company_site)
+                else:
+                    eprint('! {} is not a valid URL'.format(company_site))
+                    continue
+                    # TODO you can fall back and use name lookups with result = get_company_info(ct, company_site)
+
                 if result:
                     if not done_header:
                         # Write CSV file header - once only
@@ -237,8 +257,7 @@ if __name__ == "__main__":
                         done_header = True
                     fh.writerows(result)
                 else:
-                    eprint('Error: company {} skipped - no results'.format(company))
+                    eprint('Error: company {} skipped - no results'.format(company_site))
 
     # temp
     print(api_sc, api_cb, api_td, api_bv)
-    print(ct.call_stats())
